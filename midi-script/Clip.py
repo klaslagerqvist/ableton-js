ac8b52e64ea1bdd05b074aa0cd799719356bb405
Klas Lagerqvist <klaslagerqvist@hotmail.com>
2026-04-13 19:57:46 +0200

    fix: ensure float conversion for time parameters in get_notes_extended method

--- midi-script/Clip.py
+++ midi-script/Clip.py
@@ -28,7 +28,7 @@ class Clip(Interface):
         return ns.get_notes(from_time, from_pitch, time_span, pitch_span)
 
     def get_notes_extended(self, ns, from_time=0, from_pitch=0, time_span=99999999999999, pitch_span=128):
-        midi_note_vector = ns.get_notes_extended(from_pitch, pitch_span, from_time, time_span)
+        midi_note_vector = ns.get_notes_extended(from_pitch, pitch_span, float(from_time), float(time_span))
         return [
             {
                 "duration": note.duration,
